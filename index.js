7746484dced584d424c8faa120805f3ab4129236
omiralles <114028262+omiralles@users.noreply.github.com>
2022-09-25 23:46:08 -0400

    Corrections commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Name: Oscar Miralles Fernadez Student ID: 301250756
 
-//Create pluguin functions
+//Create pluguin
 var plugin = function (options) {
     var seneca = this;
 
@@ -9,7 +9,7 @@ var plugin = function (options) {
     });
 
     seneca.add({ role: 'product', cmd: 'get' }, function (msg, respond) {
-        this.make('product').load$(msg.data.user_id, respond);
+        this.make('product').load$(msg.data.product_id, respond);
     });
 
     seneca.add({ role: 'product', cmd: 'get-all' }, function (msg, respond) {
@@ -17,10 +17,8 @@ var plugin = function (options) {
     });
 
     seneca.add({ role: 'product', cmd: 'delete' }, function (msg, respond) {
-        this.make('product').remove$(msg.data.user_id, respond);
+        this.make('product').remove$(msg.data.product_id, respond);
     });
-
-
 }
 
 module.exports = plugin;
@@ -30,33 +28,37 @@ var seneca = require("seneca")();
 seneca.use(plugin);
 seneca.use('seneca-entity');
 
+
 //Add service functions
 seneca.add('role:api, cmd:add-product', function (args, done) {
     console.log("--> cmd:add-product");
+    console.log("> recived request");
     var product = {
         product: args.product,
         price: args.price,
         category: args.category
     }
     console.log("--> product: " + JSON.stringify(product));
-    seneca.act({ role: 'product', cmd: 'add', data: product }, function (err, msg) {
-        console.log(msg);
+    seneca.act({ role: 'product', cmd: 'add', data: product }, function (err, msg) {        
+        console.log("< sending response");
         done(err, msg);
     });
 });
 
 seneca.add('role:api, cmd:get-all-products', function (args, done) {
     console.log("--> cmd:get-all-products");
-    seneca.act({ role: 'product', cmd: 'get-all' }, function (err, msg) {
-        console.log(msg);
+    console.log("> recived request");
+    seneca.act({ role: 'product', cmd: 'get-all' }, function (err, msg) {        
+        console.log("< sending response");
         done(err, msg);
     });
 });
 
 seneca.add('role:api, cmd:get-product', function (args, done) {
     console.log("--> cmd:get-product, args.product_id: " + args.product_id);
-    seneca.act({ role: 'product', cmd: 'get', data: { product_id: args.product_id } }, function (err, msg) {
-        console.log(msg);
+    console.log("> recived request");
+    seneca.act({ role: 'product', cmd: 'get', data: { product_id: args.product_id } }, function (err, msg) {        
+        console.log("< sending response");
         done(err, msg);
     });
 });
@@ -64,13 +66,16 @@ seneca.add('role:api, cmd:get-product', function (args, done) {
 
 seneca.add('role:api, cmd:delete-product', function (args, done) {
     console.log("--> cmd:delete-product, args.product_id: " + args.product_id);
-    seneca.act({ role: 'product', cmd: 'delete', data: { product_id: args.product_id } }, function (err, msg) {
-        console.log(msg);
+    console.log("> recived request");
+    seneca.act({ role: 'product', cmd: 'delete', data: { product_id: args.product_id } }, function (err, msg) {        
+        console.log("< sending resopnse");
         done(err, msg);
     });
 });
 
 seneca.add('role:api, cmd:delete-all-products', function (args, done) {
+    console.log("> recived request");
+    console.log("< sending response");
     done(null, { cmd: "delete-all-products" });
 });
 
@@ -86,27 +91,34 @@ seneca.act('role:web', {
             'delete-product': { POST: true, },
             'delete-all-products': { POST: true, }
         }
-    }
+    }    
 })
-
-//Create requeriments for the service
-var express = require('express');
-var app = express();
-app.use(require("body-parser").json())
-app.use(seneca.export('web'));
 
 //Create counter function
 let getCounter = 0;
 let postCounter = 0;
 
-function countGetPost (req, next) {
+function countGetPost (req, res, next) {
     if (req.method === "GET") getCounter++;
     if (req.method === "POST") postCounter++;
-    console.log("Request counters ---> GET: " + getCounter + " ,POST: " + postCounter);
+    console.log("Processed request counts ---> GET: " + getCounter + " ,POST: " + postCounter);
     if (next) next();
 }
 
+//Create requeriments for the service
+var express = require('express');
+var app = express();
+//Print counter
+app.use(countGetPost);
+app.use(require("body-parser").json())
+app.use(seneca.export('web'));
+
 //Create service
 app.listen(3009)
-console.log("Server listening on localhost:3009 ...");
-
+//Print port and endpints
+console.log("Server listening on http://127.0.0.1:3009 ...");
+console.log("add-product --> http://127.0.0.1:3009/myservice/add-product?product=HotDoga&price=10&category=Meat");
+console.log("add-product --> http://127.0.0.1:3009/myservice/get-product?product_id=123");
+console.log("add-product --> http://127.0.0.1:3009/myservice/get-all-products");
+console.log("add-product --> http://127.0.0.1:3009/myservice/delete-product?product_id=123");
+console.log("add-product --> http://127.0.0.1:3009/myservice/delete-all-products");
